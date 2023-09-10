492a72a6976b3b33b02e27e555b7b4bd46ca45b1
Tipu <tipualikhan@gmail.com>
2023-09-10 21:17:02 +0530

    Initial code
    Coming soon

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Tada from "react-reveal/Tada";
+import RubberBand from "react-reveal/RubberBand";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Tada>
+        <h1>Alphonce</h1>
+      </Tada>
+      <RubberBand>
+        <h2>Coming Soon</h2>
+      </RubberBand>
+    </div>
+  );
+}
+
+export default App;
